eec600bc9eec84e17ce0d95f160b7b774adec39d
tschaffter <thomas.schaffter@gmail.com>
2020-10-16 08:51:30 -0700

    Add server folder seeded with output of openapi-generator (#6)

---
+++ server/openapi_server/controllers/health_controller.py
@@ -0,0 +1,17 @@
+import connexion
+import six
+
+from openapi_server.models.error import Error  # noqa: E501
+from openapi_server.models.health import Health  # noqa: E501
+from openapi_server import util
+
+
+def health():  # noqa: E501
+    """Get Health
+
+    Get the health of the API # noqa: E501
+
+
+    :rtype: Health
+    """
+    return 'do some magic!'
