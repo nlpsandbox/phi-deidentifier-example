4c3faeda16025905b4b40f2fed843ee3d2459ecd
Thomas Schaffter <thomas.schaffter@gmail.com>
2020-10-18 21:21:46 -0700

    Release 0.1.6 (#9)
    * Generate codebase from spec 0.1.6
    
    * Complete docker-compose requirements to deploy the stack
    
    * The server can now deidentify clinical notes!!
    
    * Update CI/CD

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # NLP Sandbox Deidentifier
 
-<!-- [![GitHub Stars](https://img.shields.io/github/stars/Sage-Bionetworks/nlp-sandbox-deidentifier.svg?color=94398d&labelColor=555555&logoColor=ffffff&style=for-the-badge&logo=github)](https://github.com/Sage-Bionetworks/nlp-sandbox-deidentifier) -->
-[![Docker Pulls](https://img.shields.io/docker/pulls/nlpsandbox/deidentifier-shiny-app.svg?color=94398d&labelColor=555555&logoColor=ffffff&style=for-the-badge&label=pulls&logo=docker)](https://hub.docker.com/r/Sage-Bionetworks/deidentifier-shiny-app)
 [![GitHub CI](https://img.shields.io/github/workflow/status/Sage-Bionetworks/nlp-sandbox-deidentifier/ci.svg?color=94398d&labelColor=555555&logoColor=ffffff&style=for-the-badge&logo=github)](https://github.com/Sage-Bionetworks/nlp-sandbox-deidentifier)
+[![GitHub Release](https://img.shields.io/github/release/Sage-Bionetworks/nlp-sandbox-deidentifier.svg?include_prereleases&color=94398d&labelColor=555555&logoColor=ffffff&style=for-the-badge&logo=github)](https://github.com/Sage-Bionetworks/nlp-sandbox-deidentifier/releases)
+[![Docker Pulls](https://img.shields.io/docker/pulls/nlpsandbox/date-annotator-example.svg?color=94398d&labelColor=555555&logoColor=ffffff&style=for-the-badge&label=pulls&logo=docker)](https://hub.docker.com/r/nlpsandbox/date-annotator-example)
 [![GitHub License](https://img.shields.io/github/license/Sage-Bionetworks/nlp-sandbox-deidentifier.svg?color=94398d&labelColor=555555&logoColor=ffffff&style=for-the-badge&logo=github)](https://github.com/Sage-Bionetworks/nlp-sandbox-deidentifier)
 
 NLP Sandbox de-identification client and server
 
 ## Specification
+
+TBA
+
+## Usage
+
+The command below starts the Deidentifier stack locally.
+
+    docker-compose up
+
+When running, the Deidentifier stacks provides a web interface (http://localhost:3838)
+that you can use to deidentify single or multiple clinical notes.
--- server/openapi_server/controllers/deidentified_notes_controller.py
+++ server/openapi_server/controllers/deidentified_notes_controller.py
@@ -1,5 +1,7 @@
 import connexion
 import six
+from flask import jsonify
+import requests
 
 from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.note import Note  # noqa: E501
@@ -11,11 +13,60 @@ def deidentified_notes_read_all(note=None):  # noqa: E501
 
     Returns the deidentified notes # noqa: E501
 
-    :param note: 
+    :param note:
     :type note: list | bytes
 
     :rtype: List[Note]
     """
+    res = []
+
+    # for testing
+    dates_url = "http://localhost:9000/api/v1/dates"
+    person_names_url = "http://localhost:9001/api/v1/person-names"
+
+    requests_session = requests.session()
+    requests_session.headers.update({'Content-Type': 'application/json'})
+    requests_session.headers.update({'charset':'utf-8'})
+
     if connexion.request.is_json:
-        note = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
-    return 'do some magic!'
+        dates = []
+        person_names = []
+        notes = connexion.request.get_json()
+
+        # Get date annotations
+        response = requests_session.post(url=dates_url, json=notes)
+        if response.status_code == 200:
+            dates = response.json()
+
+        # Get person name annotations
+        response = requests_session.post(url=person_names_url, json=notes)
+        if response.status_code == 200:
+            person_names = response.json()
+
+        # Create deidentified notes
+        for note in notes:
+            note_id = note['id']
+            res.append(deidentify_note(note,
+                [d for d in dates if d['noteId'] == note_id],
+                [d for d in person_names if d['noteId'] == note_id]))
+
+    return jsonify(res)
+
+
+def deidentify_note(note, dates, person_names):
+    """
+    Returns the deidentified clinical note where annotations are masked with '*'.
+    """
+    mask_character = '*'
+    text = note['text']
+
+    for annotation in dates:
+        mask = mask_character * annotation['length']
+        text = text[:annotation['start']] + mask + text[annotation['start'] + annotation['length']:]
+
+    for annotation in person_names:
+        mask = mask_character * annotation['length']
+        text = text[:annotation['start']] + mask + text[annotation['start'] + annotation['length']:]
+
+    note['text'] = text
+    return note
