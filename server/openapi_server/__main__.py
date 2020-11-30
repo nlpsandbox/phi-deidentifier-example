3a9b1c1592dd8076582996a5dce102cdac5be188
Thomas Schaffter <thomas.schaffter@gmail.com>
2020-11-30 13:37:12 -0800

    Fix CI/CD workflow (#19)
    * Update badges on README
    
    * Update dependabot config and CI workflow
    
    * Build the server docker image similarly to other service of the org

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import connexion
+import flask
 
 from openapi_server import encoder
 
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml', pythonic_params=True)
+
+# Configuring / to return the service information (required by NLP Sandbox)
+# app.add_url_rule('/', 'root', lambda: flask.redirect('/api/v1/service'))
+
+
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'NLP Sandbox PHI Deidentifier API'},
-                pythonic_params=True)
     app.run(port=8080)
 
 
