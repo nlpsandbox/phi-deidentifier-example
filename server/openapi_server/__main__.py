8cb2d39915c13d130ad1b7f8d9bb49b83cb76b8c
Connor Boyle <connor.boyle@sagebase.org>
2020-11-30 20:39:11 -0800

    Update Project Name

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
-                arguments={'title': 'NLP Sandbox PHI Deidentifier API'},
+                arguments={'title': 'NLP Sandbox Deidentifier API'},
                 pythonic_params=True)
     app.run(port=8080)
 
