38db1b4a6c049bbf698fb6e34cbac6e6fd9152b5
Connor Boyle <connor.boyle@sagebase.org>
2020-11-30 20:46:43 -0800

    Add Prototype Notes Deidentifier
    It just character-masks all annotation types regardless of input for now.

--- server/openapi_server/test/test_deidentified_notes_controller.py
+++ server/openapi_server/test/test_deidentified_notes_controller.py
@@ -6,29 +6,62 @@ import unittest
 from flask import json
 from six import BytesIO
 
+from openapi_server.models.deidentify_request import DeidentifyRequest  # noqa: E501
+from openapi_server.models.deidentify_response import DeidentifyResponse  # noqa: E501
 from openapi_server.models.error import Error  # noqa: E501
-from openapi_server.models.note import Note  # noqa: E501
 from openapi_server.test import BaseTestCase
 
 
 class TestDeidentifiedNotesController(BaseTestCase):
     """DeidentifiedNotesController integration test stubs"""
 
-    def test_deidentified_notes_read_all(self):
-        """Test case for deidentified_notes_read_all
+    def test_create_deidentified_notes(self):
+        """Test case for create_deidentified_notes
 
-        Get deidentified notes
+        Deidentify a clinical note
         """
-        note = null
+        deidentify_request = {
+  "note" : {
+    "noteType" : "loinc:LP29684-5",
+    "patientId" : "patientId",
+    "id" : "id",
+    "text" : "This is a text."
+  },
+  "deidentificationConfigurations" : [ {
+    "deidentificationStrategy" : {
+      "redactConfig" : "{}",
+      "annotationTypeConfig" : "{}",
+      "maskingCharConfig" : {
+        "maskingChar" : "*"
+      },
+      "dateOffsetConfig" : {
+        "offsetDays" : 0
+      }
+    },
+    "annotationTypes" : [ "text_physical_address", "text_physical_address" ]
+  }, {
+    "deidentificationStrategy" : {
+      "redactConfig" : "{}",
+      "annotationTypeConfig" : "{}",
+      "maskingCharConfig" : {
+        "maskingChar" : "*"
+      },
+      "dateOffsetConfig" : {
+        "offsetDays" : 0
+      }
+    },
+    "annotationTypes" : [ "text_physical_address", "text_physical_address" ]
+  } ]
+}
         headers = { 
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         }
         response = self.client.open(
-            '/api/v1/deidentified-notes',
-            method='GET',
+            '/api/v1/deidentifiedNotes',
+            method='POST',
             headers=headers,
-            data=json.dumps(note),
+            data=json.dumps(deidentify_request),
             content_type='application/json')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
