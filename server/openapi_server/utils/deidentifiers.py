f08ce59fa35c6d921d6eceddea6d4a44984c2e16
Connor Boyle <connor.boyle@sagebase.org>
2020-12-15 21:56:50 -0800

    Fix Masking Char Application
    Previously, it was applied to whatever the length of the annotated section happened to be when it was applied, as opposed to the length of the original text.

--- server/openapi_server/utils/deidentifiers.py
+++ server/openapi_server/utils/deidentifiers.py
@@ -13,22 +13,52 @@ def apply_masking_char(note: Note, annotations, confidence_threshold, annotation
     :param masking_char: the character used to mask PII
     :return: (note, deidentified_annotations) note with de-identified text, and annotations.
     """
-    deidentified_annotations = {annotation_type: annotation_set.copy() for annotation_type, annotation_set in annotations.items()}
     deidentified_note = Note(
         note_type=note.note_type,
         text=note.text,
         patient_id=note.patient_id,
         id=note.id
     )
+
+    left_shifts = [0] * len(note.text)
+
     for annotation_type in annotation_types:
         # Only de-identify notes with sufficient confidence level
         annotation_set = [annotation for annotation in annotations[annotation_type] if annotation['confidence'] >= confidence_threshold]
         for annotation in annotation_set:
-            mask = masking_char * annotation['length']
+            # Account for shift caused by replacement
+            start = annotation['start'] - left_shifts[annotation['start']]
+            end = annotation['start'] + annotation['length'] - left_shifts[annotation['start'] + annotation['length']]
+            length = end - start
+
+            # = "*****" * length of original text
+            mask = masking_char * len(annotation['text'])
+
+            # Replace each annotation in note with "[ANNOTATION_TYPE_HERE]"
             deidentified_note.text = \
-                deidentified_note.text[:annotation['start']] + \
-                mask + \
-                deidentified_note.text[annotation['start'] + annotation['length']:]
+                deidentified_note.text[:start] + mask + \
+                deidentified_note.text[start + length:]
+
+            # Record left shift introduced by replacement
+            # for i in range(annotation['start']+1, len(left_shifts)):
+            for i in range(annotation['start']+1, len(left_shifts)):
+                left_shifts[i] += min(i - annotation['start'] - len(mask), length - len(mask))
+
+    # Update deidentified annotations with appropriate left shifts
+    deidentified_annotations = {annotation_type: [] for annotation_type in annotations.keys()}
+    for annotation_type, annotation_set in annotations.items():
+        for annotation in annotation_set:
+            old_start = annotation['start']
+            old_end = old_start + annotation['length']
+
+            new_start = old_start - left_shifts[old_start]
+            new_end = old_end - left_shifts[old_end]
+
+            deidentified_annotation = annotation.copy()
+            deidentified_annotation['start'] = new_start
+            deidentified_annotation['length'] = new_end - new_start
+
+            deidentified_annotations[annotation_type].append(deidentified_annotation)
 
     return deidentified_note, deidentified_annotations
 
