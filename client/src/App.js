2b2870c216be129e8d1c182ef69db399cee3ee49
Connor Boyle <connor.boyle@sagebase.org>
2020-12-18 13:06:21 -0800

    Add Base React App

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="column">Left column</div>
+      <div className="column">Right column</div>
+    </div>
+  );
+}
+
+export default App;
