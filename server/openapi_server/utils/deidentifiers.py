1d9d39c03d7aab5953dd9d0e29340786f0f74e2c
Connor Boyle <connor.boyle@sagebase.org>
2020-12-17 21:51:28 -0800

    Refactor Deidentifiers

--- server/openapi_server/utils/deidentifiers.py
+++ server/openapi_server/utils/deidentifiers.py
@@ -2,6 +2,42 @@ from typing import List, Dict
 from openapi_server.models import Note
 
 
+def apply_mask(deidentified_note, annotation, left_shifts, mask):
+    # Account for shift caused by replacement
+    start = annotation['start'] - left_shifts[annotation['start']]
+    end = annotation['start'] + annotation['length'] - left_shifts[annotation['start'] + annotation['length']]
+    length = end - start
+
+
+    # Replace each annotation in note with "[ANNOTATION_TYPE_HERE]"
+    deidentified_note.text = \
+        deidentified_note.text[:start] + \
+        mask + \
+        deidentified_note.text[end:]
+
+    for i in range(annotation['start'] + 1, len(left_shifts)):
+        left_shifts[i] += min(i - annotation['start'] - len(mask), length - len(mask))
+
+
+def update_annotations(annotations, left_shifts):
+    # Update deidentified annotations with appropriate left shifts
+    deidentified_annotations = {annotation_type: [] for annotation_type in annotations.keys()}
+    for annotation_type, annotation_set in annotations.items():
+        for annotation in annotation_set:
+            old_start = annotation['start']
+            old_end = old_start + annotation['length']
+
+            new_start = old_start - left_shifts[old_start]
+            new_end = old_end - left_shifts[old_end]
+
+            deidentified_annotation = annotation.copy()
+            deidentified_annotation['start'] = new_start
+            deidentified_annotation['length'] = new_end - new_start
+
+            deidentified_annotations[annotation_type].append(deidentified_annotation)
+    return deidentified_annotations
+
+
 def apply_masking_char(note: Note, annotations, confidence_threshold, annotation_types: List[str], masking_char='*'):
     """
     Apply a masking character de-identification to a note for the given annotation types
@@ -20,45 +56,19 @@ def apply_masking_char(note: Note, annotations, confidence_threshold, annotation
         id=note.id
     )
 
-    left_shifts = [0] * len(note.text)
+    left_shifts = [0] * (len(note.text) + 1)
 
     for annotation_type in annotation_types:
         # Only de-identify notes with sufficient confidence level
         annotation_set = [annotation for annotation in annotations[annotation_type] if annotation['confidence'] >= confidence_threshold]
         for annotation in annotation_set:
-            # Account for shift caused by replacement
-            start = annotation['start'] - left_shifts[annotation['start']]
-            end = annotation['start'] + annotation['length'] - left_shifts[annotation['start'] + annotation['length']]
-            length = end - start
-
             # = "*****" * length of original text
             mask = masking_char * len(annotation['text'])
 
-            # Replace each annotation in note with "[ANNOTATION_TYPE_HERE]"
-            deidentified_note.text = \
-                deidentified_note.text[:start] + mask + \
-                deidentified_note.text[start + length:]
+            # Apply mask and record left shift introduced by replacement
+            apply_mask(deidentified_note, annotation, left_shifts, mask)
 
-            # Record left shift introduced by replacement
-            # for i in range(annotation['start']+1, len(left_shifts)):
-            for i in range(annotation['start']+1, len(left_shifts)):
-                left_shifts[i] += min(i - annotation['start'] - len(mask), length - len(mask))
-
-    # Update deidentified annotations with appropriate left shifts
-    deidentified_annotations = {annotation_type: [] for annotation_type in annotations.keys()}
-    for annotation_type, annotation_set in annotations.items():
-        for annotation in annotation_set:
-            old_start = annotation['start']
-            old_end = old_start + annotation['length']
-
-            new_start = old_start - left_shifts[old_start]
-            new_end = old_end - left_shifts[old_end]
-
-            deidentified_annotation = annotation.copy()
-            deidentified_annotation['start'] = new_start
-            deidentified_annotation['length'] = new_end - new_start
-
-            deidentified_annotations[annotation_type].append(deidentified_annotation)
+    deidentified_annotations = update_annotations(annotations, left_shifts)
 
     return deidentified_note, deidentified_annotations
 
@@ -74,7 +84,7 @@ def apply_redaction(note: Note, annotations, confidence_threshold, annotation_ty
     :return: (note, deidentified_annotations) note with de-identified text, and annotations now pointing to
             corrected character addresses.
     """
-    left_shifts = [0] * len(note.text)
+    left_shifts = [0] * (len(note.text) + 1)
 
     deidentified_note = Note(
         note_type=note.note_type,
@@ -86,35 +96,12 @@ def apply_redaction(note: Note, annotations, confidence_threshold, annotation_ty
         # Only de-identify notes with sufficient confidence level
         annotation_set = [annotation for annotation in annotations[annotation_type] if annotation['confidence'] >= confidence_threshold]
         for annotation in annotation_set:
-            # Account for shift caused by redaction
-            start = annotation['start'] - left_shifts[annotation['start']]
-            end = annotation['start'] + annotation['length'] - left_shifts[annotation['start'] + annotation['length']]
-            length = end - start
-
-            # Redact each annotation in note
-            deidentified_note.text = \
-                deidentified_note.text[:start] + \
-                deidentified_note.text[start + length:]
-
+            mask = ''
             # Record left shift introduced by redaction
-            for i in range(annotation['start'], len(left_shifts)):
-                left_shifts[i] += min(i - annotation['start'], length)
+            apply_mask(deidentified_note, annotation, left_shifts, mask)
 
     # Update deidentified annotations with appropriate left shifts
-    deidentified_annotations = {annotation_type: [] for annotation_type in annotations.keys()}
-    for annotation_type, annotation_set in annotations.items():
-        for annotation in annotation_set:
-            old_start = annotation['start']
-            old_end = old_start + annotation['length']
-
-            new_start = old_start - left_shifts[old_start]
-            new_end = old_end - left_shifts[old_end]
-
-            deidentified_annotation = annotation.copy()
-            deidentified_annotation['start'] = new_start
-            deidentified_annotation['length'] = new_end - new_start
-
-            deidentified_annotations[annotation_type].append(deidentified_annotation)
+    deidentified_annotations = update_annotations(annotations, left_shifts)
 
     return deidentified_note, deidentified_annotations
 
@@ -137,44 +124,18 @@ def apply_annotation_type(note: Note, annotations, confidence_threshold, annotat
         id=note.id
     )
 
-    left_shifts = [0] * len(note.text)
+    left_shifts = [0] * (len(note.text) + 1)
 
     for annotation_type in annotation_types:
         # Only de-identify notes with sufficient confidence level
         annotation_set = [annotation for annotation in annotations[annotation_type] if annotation['confidence'] >= confidence_threshold]
         for annotation in annotation_set:
-            # Account for shift caused by replacement
-            start = annotation['start'] - left_shifts[annotation['start']]
-            end = annotation['start'] + annotation['length'] - left_shifts[annotation['start'] + annotation['length']]
-            length = end - start
-
             # = "[ANNOTATION_TYPE_HERE]"
-            filler = "[%s]" % (annotation_type.upper(),)
-
-            # Replace each annotation in note with "[ANNOTATION_TYPE_HERE]"
-            deidentified_note.text = \
-                deidentified_note.text[:start] + \
-                filler + \
-                deidentified_note.text[end:]
-
+            mask = "[%s]" % (annotation_type.upper(),)
             # Record left shift introduced by replacement
-            for i in range(annotation['start'], len(left_shifts)):
-                left_shifts[i] += min(i - annotation['start'] - len(filler), length - len(filler))
+            apply_mask(deidentified_note, annotation, left_shifts, mask)
 
     # Update deidentified annotations with appropriate left shifts
-    deidentified_annotations = {annotation_type: [] for annotation_type in annotations.keys()}
-    for annotation_type, annotation_set in annotations.items():
-        for annotation in annotation_set:
-            old_start = annotation['start']
-            old_end = old_start + annotation['length']
-
-            new_start = old_start - left_shifts[old_start]
-            new_end = old_end - left_shifts[old_end]
-
-            deidentified_annotation = annotation.copy()
-            deidentified_annotation['start'] = new_start
-            deidentified_annotation['length'] = new_end - new_start
-
-            deidentified_annotations[annotation_type].append(deidentified_annotation)
+    deidentified_annotations = update_annotations(annotations, left_shifts)
 
     return deidentified_note, deidentified_annotations
