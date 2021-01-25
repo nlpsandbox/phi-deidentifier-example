762016a5708bc5a59f6a0ee7c0f7e56a6ea5c8bc
Connor Boyle <connor.boyle@sagebase.org>
2021-01-25 14:18:10 -0800

    Change stringSmuggler to work with Non-Latin Chars

--- client/src/stringSmuggler.js
+++ client/src/stringSmuggler.js
@@ -1,17 +1,24 @@
 // React router behaves very weirdly with any URL containing a "%" (it
 // automatically decodes them and there's nothing we can do about it). The
-// work-around here is to percent-encode every single character, then remove the
-// percent sign from each two-char chunk (e.g. "Hello" -> "%68%65%6c%6c%6f" ->
-// "68656c6c6f"), then use that as the url.
+// work-around here is to char-encode every single character in base 36 (0-9,
+// a-z), then join each code with a hyphen (e.g. "Hello" -> "2w-2t-30-30-33"),
+// then use that as the url.
 
-function encodeChar(c) {
-  return c.charCodeAt(0).toString(16);
+const base = 36;
+const divider = '-';
+
+function encodeChar(char) {
+  return char.charCodeAt(0).toString(base);
+}
+
+function decodeChar(code) {
+  return String.fromCharCode(parseInt(code, base))
 }
 
 export function encodeString(decodedString) {
-  return [...decodedString].map((char) => encodeChar(char)).join('');
+  return [...decodedString].map((char) => encodeChar(char)).join(divider);
 }
 
 export function decodeString(encodedString) {
-  return encodedString.match(/.{1,2}/g).map((chunk) => decodeURIComponent("%"+chunk)).join('');
+  return encodedString.split('-').map((code) => decodeChar(code)).join('');
 }
