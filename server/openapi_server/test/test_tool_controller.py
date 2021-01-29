118adef5b7fedc85eee30859ca7af6b409b2327a
Connor Boyle <connor.boyle@sagebase.org>
2021-01-29 11:38:01 -0800

    Add Unit Test for Tool Dependencies

--- server/openapi_server/test/test_tool_controller.py
+++ server/openapi_server/test/test_tool_controller.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 import unittest
+from unittest.mock import patch
 
 from flask import json
 from six import BytesIO
@@ -10,6 +11,7 @@ from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.tool import Tool  # noqa: E501
 from openapi_server.models.tool_dependencies import ToolDependencies  # noqa: E501
 from openapi_server.test import BaseTestCase
+from openapi_server.test.utils import mock_get_annotators_info
 
 
 class TestToolController(BaseTestCase):
@@ -30,6 +32,7 @@ class TestToolController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
+    @patch('openapi_server.utils.annotator_client.get_annotators_info', new=mock_get_annotators_info)
     def test_get_tool_dependencies(self):
         """Test case for get_tool_dependencies
 
