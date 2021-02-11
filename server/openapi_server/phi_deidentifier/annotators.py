f2459898e797a0ba0bdd752c635e01eab34f9c52
Connor Boyle <connor.boyle@sagebase.org>
2021-02-10 19:12:03 -0800

    Fix Flake8 Issues in phi_deidentifier

--- server/openapi_server/phi_deidentifier/annotators.py
+++ server/openapi_server/phi_deidentifier/annotators.py
@@ -3,6 +3,9 @@ from openapi_server.models import Note, Tool, ToolDependencies
 from openapi_server.config import Config
 
 
+__doc__ = "Utils for making requests to back end annotators"
+
+
 BASE_URLS = {
     'text_date': Config().date_annotator_api_url,
     'text_person_name': Config().person_name_annotator_api_url,
@@ -21,19 +24,26 @@ TOOL_URL_PATH = 'tool'
 
 
 def annotate(note: Note, annotation_type: str):
-    url = '%s/%s' % (BASE_URLS[annotation_type], ANNOTATION_TYPES_CAMEL_CASE[annotation_type])
+    """Get all annotations of a given type (by querying the appropriate
+    annotator) for a note.
+    """
+    url = '%s/%s' % (BASE_URLS[annotation_type],
+                     ANNOTATION_TYPES_CAMEL_CASE[annotation_type])
     response = requests.post(
         url=url,
-        json={'note': {note.attribute_map[key]: value for key, value in note.to_dict().items() if value is not None}},
+        json={'note': {note.attribute_map[key]: value for key, value in
+              note.to_dict().items() if value is not None}},
         headers={'Content-Type': 'application/json', 'charset': 'utf-8'}
     )
     return response.json()[ANNOTATION_TYPES_CAMEL_CASE[annotation_type]]
 
 
 def get_annotators_info():
+    """Get a list of Tool information objects (one for each annotator)
+    """
     tools = []
-    for annotation_type, base_url in BASE_URLS.items():
-        url = '%s/%s' % (BASE_URLS[annotation_type], TOOL_URL_PATH)
+    for base_url in BASE_URLS.values():
+        url = '%s/%s' % (base_url, TOOL_URL_PATH)
         response = requests.get(
             url=url,
             headers={'Content-Type': 'application/json', 'charset': 'utf-8'}
@@ -41,4 +51,3 @@ def get_annotators_info():
         tools.append(Tool.from_dict(response.json()))
     tool_dependencies = ToolDependencies(tool_dependencies=tools)
     return tool_dependencies
-
