6eec3df9cfe286fd49781567cac1849c5c3cadfa
tschaffter <thomas.schaffter@gmail.com>
2021-02-15 14:51:10 -0800

    Fix Tool information returned by /api/v1/tool

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -12,14 +12,15 @@ def get_tool():  # noqa: E501
     """
     tool = Tool(
         name="phi-deidentifier",
-        version="0.3.1",
+        version="1.0.0",
         license=License.APACHE_2_0,
         repository="github:nlpsandbox/phi-deidentifier",
         description="NLP Sandbox PHI Deidentifier",
         author="The NLP Sandbox Team",
         author_email="thomas.schaffter@sagebionetworks.org",
         url="https://github.com/nlpsandbox/phi-deidentifier",
-        tool_type="nlpsandbox:phi-deidentifier"
+        tool_type="nlpsandbox:phi-deidentifier",
+        tool_api_version="1.0.0"
     )
     return tool, 200
 
