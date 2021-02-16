5ba60bb74e9955f8d235c4788732d9ed340ff7f5
tschaffter <thomas.schaffter@gmail.com>
2021-02-16 12:58:18 -0800

    Re-generate codebase using edge schemas

--- server/openapi_server/models/__init__.py
+++ server/openapi_server/models/__init__.py
@@ -15,7 +15,9 @@ from openapi_server.models.masking_char_config import MaskingCharConfig
 from openapi_server.models.note import Note
 from openapi_server.models.text_annotation import TextAnnotation
 from openapi_server.models.text_date_annotation import TextDateAnnotation
+from openapi_server.models.text_date_annotation_all_of import TextDateAnnotationAllOf
 from openapi_server.models.text_person_name_annotation import TextPersonNameAnnotation
 from openapi_server.models.text_physical_address_annotation import TextPhysicalAddressAnnotation
+from openapi_server.models.text_physical_address_annotation_all_of import TextPhysicalAddressAnnotationAllOf
 from openapi_server.models.tool import Tool
 from openapi_server.models.tool_dependencies import ToolDependencies
