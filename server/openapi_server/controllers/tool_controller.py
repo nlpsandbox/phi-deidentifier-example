dd0a3d717c0e148843e85e097f98f7cbea6ddd6b
tschaffter <thomas.schaffter@gmail.com>
2021-02-25 19:59:08 -0800

    Update API and tool version in Tool object

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -12,15 +12,15 @@ def get_tool():  # noqa: E501
     """
     tool = Tool(
         name="phi-deidentifier",
-        version="1.0.0",
+        version="1.0.1",
         license=License.APACHE_2_0,
         repository="github:nlpsandbox/phi-deidentifier",
-        description="NLP Sandbox PHI Deidentifier",
+        description="Example implementation of the NLP Sandbox PHI Deidentifier",  # noqa: E501
         author="The NLP Sandbox Team",
         author_email="thomas.schaffter@sagebionetworks.org",
         url="https://github.com/nlpsandbox/phi-deidentifier",
         tool_type="nlpsandbox:phi-deidentifier",
-        tool_api_version="1.0.0"
+        tool_api_version="1.0.1"
     )
     return tool, 200
 
