a134e323ad57d13a91a27ce2749e29389244a732
Connor Boyle <connor.bo@gmail.com>
2021-07-28 11:26:18 -0700

    Fix Deidentifier Tool Type

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -2,7 +2,7 @@ from nlpsandboxclient import client
 
 from openapi_server.models.tool import Tool  # noqa: E501
 from ..config import Config
-from ..models import License, ToolDependencies
+from ..models import License, ToolDependencies, ToolType
 
 
 def get_tool():  # noqa: E501
@@ -22,7 +22,7 @@ def get_tool():  # noqa: E501
         author="NLP Sandbox Team",
         author_email="team@nlpsandbox.io",
         url="https://github.com/nlpsandbox/phi-deidentifier",
-        type="nlpsandbox:phi-deidentifier",
+        type=ToolType.PHI_DEIDENTIFIER,
         api_version="1.2.0"
     )
     return tool, 200
